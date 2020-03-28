43d6892018a3d8e60e0d0353dc99c477a248862c
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2020-03-28 02:09:12 -0300

    Aula 3 Front-end do app

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
-app.use(cors);
 app.use(express.json()); // utilizado para passar requisições json no Request Body
-
+app.use(cors());
+app.options('*',cors());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333,'127.0.0.1');
